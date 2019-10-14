c64190b119fb19cc89ce2d705ed8c6cb100069f0
Daniel Judge <dwjref@gmail.com>
2019-10-14 10:34:54 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDw4HXZen+GaglQ67+mCjDODHdHSq68tzcKjR3WBpZ+TB8P4tFKOWialBh0IuDv+Dj7S504NkDn7xN2Yjroj9oz1jJCKokHFelkL52tJgGndKOIBI1mS/DQ12pjw8wcBYWZ4bM7Edc8hRdJIUV/16vryw3HKft9ySVHDHQC9T6qdkrUEdtuKC6B8+z2P5+Tk1g24JqdDoydot7Ia1mFbmjhqPlJ9YhzIzdhqmSNC4D6AOwP5Szg0XRbhzZOAo6AnQ9Xx+JVE2I0OitZrl1xgTyFK8XBtKiBcZZnXRFUz4ku/T5oheHOR6VHq2IxM4qhG1iQq5O7ZGquga7E4rPXqR+J',
 	}  
 }
